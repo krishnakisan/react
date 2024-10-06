0b9a82e97e0c4d79c4cdfc2902d5588107ae53b4
Krishna <konw1singh@gmail.com>
2024-10-06 10:28:00 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
-/**
- <div id ="parent">
-    <div id ="child">
-        <h1>I am an h1 tag </h1
-    </div>
- </div>
- *  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
- const header = 
- React.createElement("div", {id : "parent"},
- React.createElement("div", {id: "child"},
- React.createElement("h1",{}, "I am an H1 tag"),
- )   
-
-  )
-
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(header);
+const header = React.createElement("h1",{id:"header"},"I am h1 tag");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
